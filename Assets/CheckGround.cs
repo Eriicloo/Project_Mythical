b95909fa0d5d99dc4064d2e75f3b712cb40ccc4b
Victor Romero <victor.romero.2@enti.cat>
2021-03-08 21:15:36 +0100

    Player movement and animations

---
+++ Assets/CheckGround.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    private player_controller player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<player_controller>();
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            player.grounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            player.grounded = false;
+        }
+    }
+}
