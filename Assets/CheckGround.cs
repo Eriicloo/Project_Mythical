417025d45a71bfb0dea48abbd850b1c28cc89427
Victor Romero <victor.romero.2@enti.cat>
2021-03-12 10:31:40 +0100

    Movile platform
    There's some bugs to fix, like infinite jump when you are above some platform that haves the tag "platform"

--- Assets/CheckGround.cs
+++ Assets/CheckGround.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class CheckGround : MonoBehaviour
 {
     private player_controller player;
+    private Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponentInParent<player_controller>();
+        rb2d = GetComponentInParent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            rb2d.velocity = new Vector3(0f, 0f, 0f);
+            player.transform.parent = collision.transform;
+            player.grounded = true;
+        }
     }
 
     void OnCollisionStay2D(Collision2D collision)
@@ -21,6 +33,7 @@ public class CheckGround : MonoBehaviour
 
         if (collision.gameObject.tag == "Platform")
         {
+            player.transform.parent = collision.transform;
             player.grounded = true;
         }
     }
@@ -34,6 +47,7 @@ public class CheckGround : MonoBehaviour
 
         if (collision.gameObject.tag == "Platform")
         {
+            player.transform.parent = null;
             player.grounded = true;
         }
     }
