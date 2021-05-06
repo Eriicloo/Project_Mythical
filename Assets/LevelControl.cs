ab5a39616b3fe881be60c0b180a33093a86a29f6
Eric <eric.lopez@enti.cat>
2021-05-06 09:11:45 +0200

    Small change on LevelControl script

--- Assets/LevelControl.cs
+++ Assets/LevelControl.cs
@@ -2,8 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
+
 public class LevelControl : MonoBehaviour
 {
+
+    public int index;
+    public string levelName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) 
+        {
+            SceneManager.LoadScene(index);
+
+            SceneManager.LoadScene(levelName);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
