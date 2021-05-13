b1a326350768c20ebb362ad9d1e33875a872660c
Victor Romero <victor.romero.2@enti.cat>
2021-05-13 11:31:15 +0200

    Added boss behavior

---
+++ Assets/Boss.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss : MonoBehaviour
+{
+    public int health;
+    public int damage;
+
+    public Slider healthBar;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.value = health;
+    }
+}
