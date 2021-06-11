f2847b474c25fcd08ccd72a93632eee79c709cf1
Eric <eric.lopez@enti.cat>
2021-06-11 12:57:09 +0200

    Level 3 finished (panels left!!!)

--- Assets/MusicFinder.cs
+++ Assets/MusicFinder.cs
@@ -11,10 +11,30 @@ public class MusicFinder : MonoBehaviour
     void Start()
     {
         GameObject bgm = GameObject.Find("TitleSong");
-        if (bgm) 
+        if (bgm)
         {
             musicplayer = bgm.GetComponent<MusicPlayer>();
             if (musicplayer) 
+            {
+                GetComponent<Slider>().onValueChanged.AddListener(musicplayer.updateVolume);
+            }
+        }
+
+        GameObject bgm2 = GameObject.Find("lvl1Song");
+        if (bgm2)
+        {
+            musicplayer = bgm2.GetComponent<MusicPlayer>();
+            if (musicplayer)
+            {
+                GetComponent<Slider>().onValueChanged.AddListener(musicplayer.updateVolume);
+            }
+        }
+
+        GameObject bgm3 = GameObject.Find("lvl2Song");
+        if (bgm3)
+        {
+            musicplayer = bgm3.GetComponent<MusicPlayer>();
+            if (musicplayer)
             {
                 GetComponent<Slider>().onValueChanged.AddListener(musicplayer.updateVolume);
             }
